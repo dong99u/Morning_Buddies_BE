a58295b24149fcb87018abe238e046a8378a9d33
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-06-10 09:17:53 +0900

    Docs/코드 컨벤션 추가

---
+++ src/main/java/com/morningbuddies/morningbuddies/MorningbuddiesApplication.java
@@ -0,0 +1,13 @@
+package com.morningbuddies.morningbuddies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorningbuddiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorningbuddiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morningbuddies/morningbuddies/MorningbuddiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morningbuddies.morningbuddies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorningbuddiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
