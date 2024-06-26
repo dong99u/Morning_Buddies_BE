b636c7e37c94e14f3bac08bd7a07ef49c95de2df
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-06-26 13:58:53 +0900

    Refactor/디렉토리 구조 추가

---
+++ src/main/java/com/morningbuddies/morningbuddies/domain/Member.java
@@ -0,0 +1,11 @@
+package com.morningbuddies.morningbuddies.domain;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Member {
+
+
+}
---
+++ src/main/java/com/morningbuddies/morningbuddies/domain/common/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.morningbuddies.morningbuddies.domain.common;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    private String createdBy;
+
+}
