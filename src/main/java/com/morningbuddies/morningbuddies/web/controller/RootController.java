102d09727fe2284dbf6710b800036d12d0beb412
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-06-30 16:08:41 +0900

    Feature/health api 구현

---
+++ src/main/java/com/morningbuddies/morningbuddies/web/controller/RootController.java
@@ -0,0 +1,15 @@
+package com.morningbuddies.morningbuddies.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+
+        return "I'm healthy!";
+
+    }
+}
